7ff68f9dc206c2ba7b6f0ff59f7a9ee0fbde3b9f
Nolan Conaway <nolanbconaway@gmail.com>
2019-09-04 12:53:50 -0400

    clean up db

--- traindisplay/db.py
+++ traindisplay/db.py
@@ -13,35 +13,21 @@ from sqlitedict import SqliteDict
 
 SQLITE_PATH = os.getenv("TRAIN_DISPLAY_SQLITE_PATH", ".traindisplay.sqlite")
 
-# the kws that are expected to be set
-EXPECTED_KWS = {"last_check_dt", "last_display_dt", "stops_dt"}
-
 
 def kv_store() -> SqliteDict:
     """Make a SQLite dict KV store."""
     return SqliteDict(SQLITE_PATH, autocommit=True)
 
 
-def setter(_expected=True, **kw) -> None:
-    """Set one or more database values, specified by kwargs.
-    
-    This checks that the keys are expected, unless _expected is set to False.
-    """
+def setter(**kw) -> None:
+    """Set one or more database values, specified by kwargs."""
     with kv_store() as data:
         for k, v in kw.items():
-            if _expected and k not in EXPECTED_KWS:
-                raise ValueError(f"Unknown database key: {k}.")
             data[k] = v
 
 
-def getter(k: str, default=None, _expected=True):
-    """Get a database value.
-    
-    This checks that the key is expected, unless _expected is set to False.
-    """
-    if _expected and k not in EXPECTED_KWS:
-        raise ValueError(f"Unknown database key: {k}.")
-
+def getter(k: str, default=None):
+    """Get a database value, return the default if the key is not in the database."""
     with kv_store() as data:
         return data.get(k, default)
 
@@ -59,7 +45,7 @@ def set_last_check(dt: datetime.datetime) -> datetime.datetime:
     
     This is a wrapper around the setter function, it simply knows the key name.
     """
-    setter(last_check_dt=dt)
+    return setter(last_check_dt=dt)
 
 
 def get_next_stops() -> typing.Iterable[datetime.datetime]:
@@ -75,7 +61,7 @@ def set_next_stops(dts: typing.Iterable[datetime.datetime]):
     
     This is a wrapper around the setter function, it simply knows the key name.
     """
-    setter(stops_dt=dts)
+    return setter(stops_dt=dts)
 
 
 def get_last_display() -> datetime.datetime:
@@ -92,3 +78,10 @@ def set_last_display(dt: datetime.datetime) -> datetime.datetime:
     This is a wrapper around the setter function, it simply knows the key name.
     """
     setter(last_display_dt=dt)
+
+
+def wipe() -> None:
+    """Delete all data in the dict."""
+    with kv_store() as data:
+        for key in list(data.keys()):
+            del data[key]
