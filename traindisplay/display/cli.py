5cc047352ad2201ddff46c97ed2af30d9f45cf1b
Nolan Conaway <nolanbconaway@gmail.com>
2019-09-04 21:24:02 -0400

    make text bigger, add mouse handler for rpi

--- traindisplay/display/cli.py
+++ traindisplay/display/cli.py
@@ -28,7 +28,10 @@ def main(route_id):
     """Run the main CLI Program."""
     loop = True  # for control flow later
     pygame.init()
-    pygame.mouse.set_visible(0)
+
+    # hide mouse if on rpi through fbi
+    if os.getenv("SDL_VIDEODRIVER") == "fbcon":
+        pygame.mouse.set_visible(False)
 
     # Set up the screen
     screen = pygame.display.set_mode((480, 320))
